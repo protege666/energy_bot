8ece6fe13f935c44477fc5b24a41703e4bcc53a3
vasperPy <mdmitriy2001@yandex.ru>
2021-12-04 09:43:34 +0300

    55
    зхену

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot("5051697514:AAEvyLM2LXkv54lmzTC9idXFjdt367tiKCE")
 @bot.message_handler(commands=['start'])
 def welcome_start(message):
     bot.send_message(message.chat.id, f'Приветствую тебя {message.chat.first_name} {message.chat.last_name} ')
-    bot.send_message(message.chat.id, 'Для выполнения заданий пройдите регистрацию!')
+    bot.send_message(message.chat.id, 'Для выполнения заданий пройдите регистрацию!!!!!!!')
 
 
 bot.infinity_polling()
