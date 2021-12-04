619ff1d808857ffca5f68d9839ffe1953b840a9f
protege666 <vkrasnikov2002@gmail.com>
2021-12-04 13:04:42 +0300

    fix bug's

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from emoji import emojize
 from registration import *
 
 bot = telebot.TeleBot("5051697514:AAEvyLM2LXkv54lmzTC9idXFjdt367tiKCE")
+smile = [emojize(':wink:', use_aliases=True), emojize(':exclamation:', use_aliases=True)]
 
 @bot.message_handler(commands=['start'])
 def welcome_start(message):
-    smile = emojize('U+1F609', use_aliases=True)
-    bot.send_message(message.chat.id, f'Приветствую тебя {message.chat.first_name} {message.chat.last_name}')
-    bot.send_message(message.chat.id, 'Для выполнения заданий пройдите регистрацию! (Команда /reg)')
-
+    bot.send_message(message.chat.id, f'Приветствую тебя, {message.chat.first_name} {message.chat.last_name}')
+    bot.send_message(message.chat.id, f'Для выполнения заданий пройдите регистрацию{smile[0]} \n\n(Команда /reg)')
+    global telegram_id
+    telegram_id = message.from_user.id
 
 @bot.message_handler(commands=['check'])
 def check_db(message):
@@ -19,20 +20,20 @@ def check_db(message):
 
 @bot.message_handler(commands=['reg'])
 def registration(message):
-    bot.send_message(message.chat.id, 'Укажите свое имя: ')
-    bot.register_next_step_handler(message, get_name)
-
-
-def get_name(message):
-    global name
-    name = message.text
-    bot.send_message(message.from_user.id, 'Укажите свою фамилию: ')
+    bot.send_message(message.chat.id, 'Укажите свою фамилию: ')
     bot.register_next_step_handler(message, get_surname)
 
 
 def get_surname(message):
     global surname
     surname = message.text
+    bot.send_message(message.from_user.id, 'Укажите свое имя: ')
+    bot.register_next_step_handler(message, get_name)
+
+
+def get_name(message):
+    global name
+    name = message.text
     bot.send_message(message.from_user.id, 'Укажите свое отчество: ')
     bot.register_next_step_handler(message, get_patronymic)
 
@@ -45,30 +46,55 @@ def get_patronymic(message):
 
 
 def get_post(message):
-    markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-    btn1 = types.KeyboardButton('Да')
-    btn2 = types.KeyboardButton('Нет')
-    markup.add(btn1, btn2)
+    global telegram_id
+    telegram_id = message.from_user.id
+    markup_inline = types.InlineKeyboardMarkup()
+    item_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
+    item_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
+    markup_inline.add(item_yes, item_no)
     global post
+    global msg
     post = message.text
     bot.send_message(message.from_user.id,
-                     f'Проверьте свои данные!\nИмя: {name}, Фамилия: {surname}, Отчество: {patronymic}, Должность: {post}')
-    bot.send_message(message.from_user.id, 'Все верно?', reply_markup=markup)
-    bot.register_next_step_handler(message, check)
+                     f'{smile[1]}Проверьте свои данные{smile[1]}\n\nФамилия: {surname}\nИмя: {name}\nОтчество: {patronymic}\nДолжность: {post}')
+    msg = bot.send_message(message.chat.id, 'Все верно?', reply_markup=markup_inline)
+   # bot.register_next_step_handler(message, check)
 
 
-def check(message):
+"""def check(message):
     text = message.text
+    print(telegram_id)
     if text == "Да":
         markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
         btn1 = types.KeyboardButton('Далее')
         markup.add(btn1)
-        sql(name, surname, patronymic, post)
-        bot.send_message(message.from_user.id, 'Вы успешно зарегистрировались!',reply_markup=markup)
+        sql(telegram_id, surname, name, patronymic, post)
+        bot.send_message(message.from_user.id, 'Вы успешно зарегистрировались!', reply_markup=markup)
     else:
         bot.send_message(message.from_user.id, 'Пройдите регистрацию еще раз /reg')
-        bot.register_next_step_handler(message, registration)
+        bot.register_next_step_handler(message, registration)"""
 
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    if call.message:
+        if call.data == 'yes':
+            markup_inline = types.InlineKeyboardMarkup()
+            profile = types.InlineKeyboardButton(text='Профиль', callback_data='profile')
+            firsttest = types.InlineKeyboardButton(text='Пройти начальное тестирование', callback_data='first_test')
+            markup_inline.add(profile, firsttest)
+            sql(telegram_id, surname, name, patronymic, post)
+            bot.send_message(call.message.chat.id, 'Вы успешно зарегистрировались!', reply_markup=markup_inline)
+            bot.edit_message_text("result", chat_id=call.message.chat.id, message_id=msg.message_id)
+            bot.delete_message(call.message.chat.id, msg.message_id)
+        elif call.data == 'no':
+            bot.send_message(call.message.chat.id, 'Пройдите регистрацию еще раз /reg')
+            bot.edit_message_text("result", chat_id=call.message.chat.id, message_id=msg.message_id)
+            bot.delete_message(call.message.chat.id, msg.message_id)
+        elif call.data == "profile":
+            pass
+        elif call.data == "first_test":
+            pass
 
 
 
--- registration.py
+++ registration.py
@@ -15,19 +15,19 @@ def add(name, surname, patronymic, post, module1=0, module2=0, module3=0):
     connect().executemany("INSERT INTO user VALUES (?,?,?,?,?,?,?)", list)
     conn.commit()"""
 
-def sql(name, surname, patronymic, post, module1=0, module2=0, module3=0):
+def sql(telegram_id, surname, name, patronymic, post, module1=0, module2=0, module3=0, module4=0, module5=0, module6=0, module7=0):
     conn = sqlite3.connect("database.db")
     cursor = conn.cursor()
     try:
-        cursor.execute("""CREATE TABLE user
-                             (name text, surname text, patronymic text,
-                              post text, module1 int, module2 int, module3 int)
+        cursor.execute("""CREATE TABLE users
+                             (telegram_id text ,surname text, name text, patronymic text,
+                              post text, module1 int, module2 int, module3 int, module4 int, module5 int, module6 int, module7 int)
                           """)
     except:
         print("таблица уже создана!")
 
-    list = [(f'{name}', f'{surname}', f'{patronymic}', f'{post}', f'{module1}', f'{module2}', f'{module3}')]
-    cursor.executemany("INSERT INTO user VALUES (?,?,?,?,?,?,?)", list)
+    list = [(f'{telegram_id}', f'{surname}', f'{name}', f'{patronymic}', f'{post}', f'{module1}', f'{module2}', f'{module3}', f'{module4}', f'{module5}', f'{module6}', f'{module7}')]
+    cursor.executemany("INSERT INTO users VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", list)
     conn.commit()
 
 
